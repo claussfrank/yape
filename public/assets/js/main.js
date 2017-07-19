52b62c64bd065769fe31a62345bfee1c0effa3aa
Clauss Franco <you@example.com>
2017-07-19 14:58:21 -0500

    Segunda parte de Maquetación

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,12 @@
- $(document).ready(function(){
-       $('.carousel.carousel-slider').carousel({fullWidth: true});
-    });
+var cargarPagina = function(){
+	$('.carousel.carousel-slider').carousel({fullWidth: true});
+};
+//$post('http://localhost:8080/api/registerNumber',{
+//	"phone":"+51986161136",
+//	"terms":true
+//}).then(function(response){
+//	console.log(response);
+//}).catch(function(error){
+//	console.log(error);
+//})
+$(document).ready(cargarPagina);
